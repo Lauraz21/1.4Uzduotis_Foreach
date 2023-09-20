3eafe1ffeb776f41b79978cd1e18c79dcf4b7a2b
Laura <Laura@DESKTOP-BESSB6E>
2023-09-20 21:34:46 +0300

    Add project files.

---
+++ 1.4Uzduotis_Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1._4Uzduotis_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            string[] result = ReturnSentence(sentence);
+            
+            foreach (string tot in result)
+            {
+                Console.WriteLine(tot);
+            }
+
+        }
+        public static string[] ReturnSentence(string sentence)
+        {
+            string[] newSentence = sentence.Split(' ');
+            string[] sentenceReturn = new string[sentence.Length];
+            int index = 0;
+            foreach (string item in newSentence)
+            {
+                if (item.Length >= 5)
+                {
+                    sentenceReturn[index] = item;
+                    index++;
+                }
+            }
+            return sentenceReturn;
+        }
+    }
+}
